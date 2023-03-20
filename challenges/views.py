8e1b34f60b798dc0659d7c8f03d55bb4164d7e8b
Will <willj4945@gmail.com>
2023-03-20 14:32:21 -0400

    Updated logic from giant if/elif to a dictionary with month:challenge.

--- challenges/views.py
+++ challenges/views.py
@@ -1,34 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for 20 minutes each day",
+    "march": "Walk for 20 minutes each day",
+    "april": "Walk for 20 minutes each day",
+    "may": "Walk for 20 minutes each day",
+    "june": "Walk for 20 minutes each day",
+    "july": "Walk for 20 minutes each day",
+    "august": "Walk for 20 minutes each day",
+    "september": "Walk for 20 minutes each day",
+    "october": "Walk for 20 minutes each day",
+    "november": "Walk for 20 minutes each day",
+    "december": "Walk for 20 minutes each day",
+}
 
 
 # Create your views here.
+def monthly_challenge_by_num(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Month does not exist.")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
 
 def monthly_challenge(request, month):
-    if month == "january":
-        challenge_text = "Eat no meat for the entire month!"
-    elif month == 'february':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'march':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'april':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'may':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'june':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'july':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'august':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'september':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'october':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'november':
-        challenge_text = "Walk for 20 mins each day."
-    elif month == 'december':
-        challenge_text = "Walk for 20 mins each day."
-    else:
-        return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+
+    except:
+        return HttpResponseNotFound("This month does not exist.")
